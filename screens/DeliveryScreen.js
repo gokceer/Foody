d66bedf58138db4090c1187b56f3f1f530d57694
gokceer <gkcer@hotmail.com>
2022-11-24 12:34:43 +0300

    feat: mapView implementation

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -6,6 +6,7 @@ import { selectRestaurant } from "../slices/restaurantSlice";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { XMarkIcon } from "react-native-heroicons/solid";
 import * as Progress from "react-native-progress";
+import MapView, { Marker } from "react-native-maps";
 
 const DeliveryScreen = () => {
   const navigation = useNavigation();
@@ -20,9 +21,9 @@ const DeliveryScreen = () => {
           <Text className="font-light text-lg text-white">Order Help</Text>
         </View>
 
-        <View className="bg-white mx-5 my-2 p-6 rounded-md z-50 shadow-md">
+        <View className="bg-white mx-5 my-2 p-6 rounded-md shadow-md z-50">
           <View className="flex-row justify-between">
-            <View className="">
+            <View>
               <Text className="text-lg text-gray-400">Estimated Arrival</Text>
               <Text className="text-3xl font-bold">45-55 Minutes</Text>
             </View>
@@ -38,6 +39,41 @@ const DeliveryScreen = () => {
             Your order at {restaurant.title} is being prepared
           </Text>
         </View>
+      </SafeAreaView>
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.long,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        style={{
+          marginTop: -40,
+          flex: 1,
+          zIndex: -1,
+        }}
+        mapType="mutedStandard"
+      >
+        <Marker
+          coordinate={{ latitude: restaurant.lat, longitude: restaurant.long }}
+          title={restaurant.title}
+          description={restaurant.shortDes}
+          identifier="origin"
+          pinColor="#00CCBB"
+        />
+      </MapView>
+      <SafeAreaView className="bg-white flex-row items-center pb-6 pl-2 space-x-5 h-20">
+        <Image
+          source={{
+            uri: `https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450`,
+          }}
+          className="w-7 h-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className=" text-xl">Gokce Er</Text>
+          <Text className="text-gray-400">Your Rider!</Text>
+        </View>
+        <Text className="text-[#00CCBB] text-lg mr-5 font-bold">Call</Text>
       </SafeAreaView>
     </View>
   );
